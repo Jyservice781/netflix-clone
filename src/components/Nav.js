2dc119ffca3c87f99bae5794e07471ebbbb51a94
Jyservice781 <gimjuyeong512@gmail.com>
2024-09-04 22:56:05 +0900

    router update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,35 +1,51 @@
-import React, {useEffect, useState} from 'react'
-import "./Nav.css"
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Nav() {
-   const [show, setShow]= useState(false);
+  const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
-   useEffect(()=> {
+  useEffect(() => {
     window.addEventListener("scroll", () => {
-      if(window.scrollY > 50){
-        
-         setShow(true);
+      if (window.scrollY > 50) {
+        setShow(true);
       } else {
         setShow(false);
       }
-    })
+    });
     return () => {
-       window.removeEventListener("scroll", () => {})
-    }
-   }, []);
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
+  const handleChange = (e) => {
+     setSearchValue(e.target.value)
+     navigate(`/search?q=${e.target.value}`)
+     
+  };
 
   return (
     <nav className={`nav ${show && "nav_black"}`}>
       <img
-        alt="Netflix logo" 
+        alt="Netflix logo"
         src="https://upload.wikimedia.org/wikipedia/commons/f/fd/Netflix-Logo.png"
-        className="nav_logo" onClick={() => window.location.reload()}
+        className="nav_logo"
+        onClick={() => window.location.reload()}
       />
-      <img 
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        placeholder="영화를 검색해주세요"
+        type="text"
+      />
+      <img
         alt="User logged"
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmX1IYrleu5pZkTWvD6cBrp4E0knysir8f-A&s"
         className="nav_avatar"
       />
     </nav>
-  )
+  );
 }
